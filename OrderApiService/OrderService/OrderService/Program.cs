d74f8bfc8269eb7c4a96c4759385e8fa7ccf6290
jullianchris <jullianchristopher6@gmail.com>
2024-01-15 22:41:27 +0530

    report service added to communicate with rabbitmq with order and monolothic app

--- OrderApiService/OrderService/OrderService/Program.cs
+++ OrderApiService/OrderService/OrderService/Program.cs
@@ -1,20 +1,50 @@
+using OrderService;
+using Plain.RabbitMQ;
+using RabbitMQ.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Order service",
+        Version = "v1"
+    });
+});
+
+var connectionString = builder.Configuration["ConnectionString"];
+builder.Services.AddSingleton<IOrderDetailsProvider>(new OrderDetailsProvider(connectionString));
+builder.Services.AddSingleton<IOrderCreator>(x => new OrderCreator(connectionString, x.GetService<ILogger<OrderCreator>>()));
+builder.Services.AddSingleton<IOrderDeletor>(new OrderDeletor(connectionString));
+
+builder.Services.AddSingleton<IConnectionProvider>(new ConnectionProvider("amqp://guest:guest@localhost:5672"));
+builder.Services.AddSingleton<IPublisher>(x => new Publisher(x.GetService<IConnectionProvider>(),
+        "order_exchange",
+        ExchangeType.Topic));
+builder.Services.AddSingleton<ISubscriber>(x => new Subscriber(x.GetService<IConnectionProvider>(),
+    "inventory_exchange",
+    "inventory_response",
+    "inventory.response",
+    ExchangeType.Topic));
+
+builder.Services.AddHostedService<InventoryResponseListener>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order Service");
+});
 
 app.UseHttpsRedirection();
 
--- OrderApiService/OrderService/OrderService/obj/Debug/net7.0/OrderService.AssemblyInfo.cs
+++ OrderApiService/OrderService/OrderService/obj/Debug/net7.0/OrderService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+526721a42d6f26ab215a01d1598c24ca9a0cc6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
